7889a64425864e436ae19901fcc1bc0eeb043d40
pravinchandankhede <pravinchandankhede@hotmail.com>
2025-09-02 11:15:39 +0530

    added a blade loader component

---
+++ src/xblade/xbladeshell/src/blade-loader.component.ts
@@ -0,0 +1,46 @@
+import { Component, Input, OnInit, ViewChild, ElementRef, Injector, NgModuleFactory, Compiler, Type } from '@angular/core';
+
+// Metadata interface for blade/component
+export interface BladeMeta {
+  type: 'json' | 'js' | 'knockout' | 'react' | 'angular';
+  entry: string; // Path or module/component name
+  props?: any;
+}
+
+@Component({
+  selector: 'app-blade-loader',
+  template: '<div #bladeContainer></div>'
+})
+export class BladeLoaderComponent implements OnInit {
+  @Input() bladeMeta!: BladeMeta;
+  @ViewChild('bladeContainer', { static: true }) container!: ElementRef;
+
+  constructor(private injector: Injector, private compiler: Compiler) {}
+
+  async ngOnInit() {
+    switch (this.bladeMeta.type) {
+      case 'json':
+        // TODO: Load and render JSON template using your dynamic renderer
+        break;
+      case 'js':
+        // TODO: Dynamically import and execute JS, attach to container
+        // Example: const module = await import(this.bladeMeta.entry);
+        break;
+      case 'knockout':
+        // TODO: Load Knockout.js, view model, and template, then apply bindings
+        // Example: ko.applyBindings(viewModel, this.container.nativeElement);
+        break;
+      case 'react':
+        // TODO: Use ReactDOM.render to mount React component in container
+        // Example: ReactDOM.render(<Component {...this.bladeMeta.props} />, this.container.nativeElement);
+        break;
+      case 'angular':
+        // TODO: Dynamically load Angular module/component
+        // Example: const moduleFactory = await import(this.bladeMeta.entry);
+        // Use compiler to create and inject component
+        break;
+      default:
+        throw new Error('Unsupported blade type');
+    }
+  }
+}
